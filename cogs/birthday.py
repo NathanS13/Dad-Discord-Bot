47b6cb95e8ce38551249383eb2eaa9f4df7c31d6
Nathan Schaefer <ildledx@gmail.com>
2024-04-06 15:32:12 -0600

    bug fixes

--- cogs/birthday.py
+++ cogs/birthday.py
@@ -10,6 +10,10 @@ class Birthday_Bot(commands.Cog):
     def __init__(self, bot):
         print('init')
         self.bot = bot
+        self.birthday_check.start
+
+    def cog_unload(self):
+        self.birthday_check.cancel()
 
     #@commands.command(name='request')
     #async def plex_request(self, ctx):
@@ -28,11 +32,11 @@ class Birthday_Bot(commands.Cog):
     #        file.write(f'{request}\n')
     #        await self.alert_plex_admins(request)
 
-    @tasks.loop(hours=1)
+    @tasks.loop(seconds=5.0)
     async def birthday_check(self):
         channel = self.bot.get_channel(995062169894916177)
-        birthdays = self.check_birthdays()
-        if datetime.now().hour == 14 and len(birthdays > 0):
+        birthdays = self.generate_birthday_list()
+        if datetime.now().hour == 14 and len(birthdays) > 0:
             print('We have some birthdays!', birthdays)
             for id in birthdays:
                 birthday_boy = channel.guild.get_member(id).mention
@@ -40,25 +44,28 @@ class Birthday_Bot(commands.Cog):
                 await channel.send(f"Happy Birthday {birthday_boy}!")
                 await channel.send(f"Wow kiddo already {age} years old.")
 
-    def check_birthdays(self):
+    def generate_birthday_list(self):
         user_list = utils.batch_user_list(os.path.join(os.getcwd(), 'the_boys.json'), 'discord_id', None)
         birthday_list = []
         for id in user_list:
             year = utils.parse_data(os.path.join(os.getcwd(), 'the_boys.json'), id, 'dob_year')
             month = utils.parse_data(os.path.join(os.getcwd(), 'the_boys.json'), id, 'dob_month')
             day = utils.parse_data(os.path.join(os.getcwd(), 'the_boys.json'), id, 'dob_day')
-            
             today = datetime.now()
-
             if day == today.day and month == today.month:
                 birthday_list.append(id)
                 utils.replace_value(os.path.join(os.getcwd(), 'the_boys.json'), id, 'age', (today.year-year))
 
         return birthday_list
-        
+
+    @birthday_check.before_loop
+    async def before_birthday_check(self):
+        print('birthday check waiting...')
+        await self.bot.wait_until_ready()
+
     @commands.command(name='birthday6')
     async def birthday_test(self, ctx):
-        self.check_birthdays()
+        await self.birthday_check()
 
 async def setup(bot):
     await bot.add_cog(Birthday_Bot(bot))
