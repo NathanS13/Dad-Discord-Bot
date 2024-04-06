69703e108764f5cf5d7b239f89c9e0501ab14ac8
Nathan Schaefer <ildledx@gmail.com>
2024-04-06 15:55:18 -0600

    fixes

--- cogs/birthday.py
+++ cogs/birthday.py
@@ -1,6 +1,6 @@
 import os
 import asyncio
-from datetime import datetime
+import datetime
 from discord.ext import commands, tasks
 
 from src import utils
@@ -46,7 +46,7 @@ class Birthday_Bot(commands.Cog):
         print('debug!')
         channel = self.bot.get_channel(995062169894916177)
         birthdays = self.generate_birthday_list()
-        if datetime.now().hour == 14 and len(birthdays) > 0:
+        if datetime.datetime.now().hour == 14 and len(birthdays) > 0:
             print('We have some birthdays!', birthdays)
             for id in birthdays:
                 birthday_boy = channel.guild.get_member(id).mention
@@ -62,7 +62,7 @@ class Birthday_Bot(commands.Cog):
             year = utils.parse_data(os.path.join(os.getcwd(), 'the_boys.json'), id, 'dob_year')
             month = utils.parse_data(os.path.join(os.getcwd(), 'the_boys.json'), id, 'dob_month')
             day = utils.parse_data(os.path.join(os.getcwd(), 'the_boys.json'), id, 'dob_day')
-            today = datetime.now()
+            today = datetime.datetime.now()
             if day == today.day and month == today.month:
                 birthday_list.append(id)
                 utils.replace_value(os.path.join(os.getcwd(), 'the_boys.json'), id, 'age', (today.year-year))
